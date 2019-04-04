7b4b2bcdf28b63f255443e48eef3fbbcc880bad6
Pravin Lokhande <pravin.lokhande@cuelogic.com>
2019-04-04 12:25:28 +0530

    pravin's system IP added to cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins ['localhost:4200','14.141.151.78:4200']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
