fb0c66e78d431ead7af23ca4f4776430c42b9cde
Pravin Lokhande <pravin.lokhande@cuelogic.com>
2019-04-04 11:43:10 +0530

    migration fixed.

--- db/migrate/20190325144011_add_devise_columns_to_users.rb
+++ db/migrate/20190325144011_add_devise_columns_to_users.rb
@@ -2,7 +2,7 @@ class AddDeviseColumnsToUsers < ActiveRecord::Migration[5.2]
 
   def change
 
-    # add_column :users, :email, :string
+    add_column :users, :email, :string
     add_column :users, :uuid, :string
     add_column :users, :encrypted_password, :string, null: false, default: ""
     add_column :users, :reset_password_token, :string    
